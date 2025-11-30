0e5c60da4b46fc8c71eb7925d0ed2344a10b6c4c
GitHub Action <voasd00@gmail.com>
2025-11-30 21:51:34 +0700

    feat: adding lottery contract using simple randomness

---
+++ contracts/RandomGenerator.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract RandomGenerator {
+    // Hàm sinh số ngẫu nhiên
+    function generateRandomIndex(uint _ticketCount) external view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, _ticketCount))) % _ticketCount;
+    }
+}
